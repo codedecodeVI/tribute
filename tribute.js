c5c0db6936e7d2ff96bc53b55f0232793da2289a
Prashanth Reddy <prashanthdreddy@users.noreply.github.com>
2017-06-02 13:02:30 -0700

    first_build

---
+++ tribute.js
@@ -0,0 +1,31 @@
+$(document).ready(function(){
+   $("p, q").fadeOut(1000); 
+    $("p, q").show(1000);
+    
+    /*$("button").click(function() {
+        $(".div1").load("ajax.txt", function(responseTxt, statusTxt, xhr) {
+            if(statusTxt == "success") 
+                alert("File loaded successfully");
+                if(statusTxt == "error")
+                    alert("Error: " + xhr.status + ':' + xhr.statusText);
+            });
+    });*/
+    
+   /* $("button").click(function() {
+     $.get("ajax.txt",  function(data, value) {
+       alert("Data: " + data + "Value: " + value); 
+    });   
+    });*/
+    
+    $("button").click(function() {
+       $.post("demo_test.asp", 
+             {
+           name: "Prashanth Reddy",
+           wifeName: "Priyanka Diddi",
+           city: "Hyderabad"
+       },
+              function(data, status) {
+           alert("Data: " + data + "\nStatus " + status);
+       }); 
+    });
+});
